9fb563f56128edf3fb472d5f86e4cfc6733c02e4
oleksiipodorozhko <oleksii.podorozhko@gmail.com>
2025-08-24 16:27:00 +0300

    Finish task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,31 +1,13 @@
-console.log("Task 1");
+const categories = document
+  .querySelector('ul#categories')
+  .querySelectorAll('.item');
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-};
+console.log(`Number of categories: ${categories.length}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+categories.forEach(category => {
+  const header = category.querySelector('h2');
+  const items = category.querySelectorAll('li');
+
+  console.log(`Category: ${header.textContent}`);
+  console.log(`Elements: ${items.length}`);
+})
