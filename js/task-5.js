555b0489574fe5bfa878be2a741e76cde9889bae
oleksiipodorozhko <oleksii.podorozhko@gmail.com>
2025-08-28 22:48:05 +0300

    Finish task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorLabel = document.querySelector('.color');
+
+
+button.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorLabel.textContent = color;
+});
